499bb249acfe0a108959f66812a29c0aa88706aa
rocopateo <154349882+rocopateo@users.noreply.github.com>
2025-06-09 22:11:19 +0200

    Update site info

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -39,9 +39,9 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    website: 'https://example.com',
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    website: 'https://rocopateo.github.io',
+    title: 'rocopateo`s netspace',
+    subtitle: 'Bienvenido a mi repositorio de hipermedia personal',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
@@ -73,36 +73,23 @@ const siteConfig: SiteConfig = {
         {
             text: 'Contact',
             href: '/contact'
-        },
-        {
-            text: 'Terms',
-            href: '/terms'
-        },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
         }
-    ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
-        {
             text: 'Instagram',
-            href: 'https://instagram.com/'
+            href: 'https://www.instagram.com/rocolinne/'
         },
         {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
+            text: 'Email',
+            href: 'rocopateo123@gmail.com'
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: '',
+        text: "",
         image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
+            src: '/Jorogumo.jpg',
+            alt: 'The Jorōgumo, a captivating and eerie entity from Japan’s Edo period folklore, embodies a unique blend of seduction and menace. Derived from the Japanese terms "Jorō" meaning "to weave," and "kumo" meaning "spider," the name literally translates to "weaving spider."⁣'
         },
         actions: [
             {
